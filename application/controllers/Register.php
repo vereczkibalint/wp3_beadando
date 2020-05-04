f4a5e348c88b53b33a52f049670b0b9da8fb5d56
vereczkibalint <vereczkibalint@gmail.com>
2020-05-04 20:52:52 +0200

    created register controller

---
+++ application/controllers/Register.php
@@ -0,0 +1,46 @@
+<?php
+class Register extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+        $this->load->library('ion_auth');
+    }
+    
+    public function index(){
+        $this->form_validation->set_rules('first_name', 'Keresztnév','required');
+        $this->form_validation->set_rules('last_name', 'Vezetéknév','required');
+        $this->form_validation->set_rules('username','Felhasználónév','required|is_unique[users.username]');
+        $this->form_validation->set_rules('email','Email','valid_email|required|is_unique[users.email]');
+        $this->form_validation->set_rules('password','Jelszó','min_length[8]|required');
+        $this->form_validation->set_rules('confirm_password','Jelszó újra','matches[password]|required');
+        
+        $view_params = [];
+        
+        if($this->form_validation->run() === FALSE){
+            $this->load->helper('form');
+            $this->load->view('register/index', $view_params);
+        }else{
+            $firstname = $this->input->post('first_name');
+            $lastname = $this->input->post('last_name');
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            $email = $this->input->post('email');
+            
+            $additional_data = [
+                'first_name' => $firstname,
+                'last_name' => $lastname
+            ];
+            
+            if($this->ion_auth->register($username, $password, $email, $additional_data)){
+                $_SESSION['auth_message'] = "Sikeres regisztráció!";
+                $this->session->mark_as_flash('auth_message');
+                redirect('auth/login');
+            }else{
+                $_SESSION['auth_message'] = $this->ion_auth->errors();
+                $this->session->mark_as_flash('auth_message');
+                redirect('register');
+            }
+        }
+    }
+}
