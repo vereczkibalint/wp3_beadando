2d07a29d81d8efe5affe075ce1b0576cba051be9
vereczkibalint <vereczkibalint@gmail.com>
2020-05-04 20:53:42 +0200

    edited login page to make a custom form

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,28 +1,29 @@
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
-
-<div id="infoMessage"><?php echo $message;?></div>
-
-<?php echo form_open("auth/login");?>
-
-  <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
-
-
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
-
-<?php echo form_close();?>
-
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+<?php $this->load->view('layout/header'); ?>
+<?php $this->load->view('layout/navbar'); ?>
+<div class='text-center'>
+    <?php echo form_open('login', array('class'=>'login-form'));?>
+        <h1 class="h3 mb-3 font-weight-normal">Bejelentkezés</h1>
+        <?php echo form_label('Email:','email', array('class'=>'sr-only'));?>
+        <?php echo form_input('email',set_value('email'), array(
+            'type'=>'email',
+            'id'=>'email',
+            'class' => 'form-control',
+            'placeholder'=>'Email cím'
+        ));?>
+        <?php echo form_error('email');?>
+<br>
+        <?php echo form_label('Password:', 'password', array('class'=>'sr-only'));?>
+        <?php echo form_password('password','', array(
+            'type'=>'password',
+            'id'=>'password',
+            'class' => 'form-control',
+            'placeholder'=>'Jelszó'
+        ));?>
+        <?php echo form_error('password');?>
+<br>
+        <?php echo form_submit('login','Bejelentkezés',array(
+            'class'=>'btn btn-lg btn-primary btn-block'
+        )); ?>
+    <?php echo form_close();?>
+</div>
+<?php $this->load->view('layout/footer'); ?>
