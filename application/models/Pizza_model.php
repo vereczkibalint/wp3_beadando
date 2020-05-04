e1a8aa924ddc0b2836a2f3e6fb185af17902cde8
vereczkibalint <vereczkibalint@gmail.com>
2020-05-04 17:17:36 +0200

    basic pizza model created

---
+++ application/models/Pizza_model.php
@@ -0,0 +1,47 @@
+<?php
+class Pizza_model extends CI_Model{
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    public function get_list(){
+        $this->db->select('items.id, items.category_id, items.name, items.price, items.image, item_categories.category_name');
+        $this->db->from('items');
+        $this->db->join('item_categories', 'items.category_id = item_categories.category_id');
+        $this->db->order_by('id', 'ASC');
+        
+        $query = $this->db->get();
+        
+        $result = $query->result();
+        
+        return $result;
+    }
+    
+    public function select_by_id($id){
+        $this->db->select('item.id, item.category_id, item.name, item.price, item.image, item_categories.category_name');
+        $this->db->from('items');
+        $this->db->join('item_categories', 'item.category_id = item_categories.category_id');
+        $this->db->where('item.id', $id);
+        
+        return $this->db->get()->row();
+    }
+    
+    public function insert($item){
+        $query = $this->db->get_where('items', ['name' => $item['name']]);
+        
+        $item_count = $query->num_rows();
+        
+        if($item_count > 0){
+            return -1;
+        }else{
+            $this->db->insert('items', $item);
+            return $this->db->insert_id();
+        }
+    }
+    
+    public function delete($id){
+        $this->db->where('id', $id);
+        return $this->db->delete('items');
+    }
+}
