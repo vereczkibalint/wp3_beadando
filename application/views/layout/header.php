9997ee44543dc61ee8f64497eed1f29998194c74
vereczkibalint <vereczkibalint@gmail.com>
2020-05-04 17:18:18 +0200

    basic template created

---
+++ application/views/layout/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PizzaShop</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?=base_url('assets/css/bootstrap.min.css')?>">
+    <link rel="stylesheet" href="<?=base_url('assets/css/custom.css')?>">
+</head>
+<body>
+<main class="container">
---
+++ application/views/layout/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-pink">
+  <a class="navbar-brand" href="#">PizzaShop</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#pizzaNavbar" aria-controls="pizzaNavbar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="pizzaNavbar">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Összes termék</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item mr-2">
+        <a href="#" class="nav-link">
+          <i class="fas fa-shopping-cart"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link font-weight-bold">Bejelentkezés</a>
+      </li>
+    </ul>
+  </div>
+</nav>
