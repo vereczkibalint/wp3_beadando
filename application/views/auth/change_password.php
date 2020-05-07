30b8654fcde04cf3f48ac081fe9b2a55f99f1702
vereczkibalint <vereczkibalint@gmail.com>
2020-05-07 14:56:17 +0200

    password change site got some styling

--- application/views/auth/change_password.php
+++ application/views/auth/change_password.php
@@ -1,25 +1,30 @@
-<h1><?php echo lang('change_password_heading');?></h1>
+<?php $this->load->view('layout/header'); ?>
+<?php $this->load->view('layout/navbar'); ?>
+<div class="mt-5 passwordChangeBox text-center">
+<h1 class="mb-3"><?php echo lang('change_password_heading');?></h1>
 
-<div id="infoMessage"><?php echo $message;?></div>
+    <div id="infoMessage"><?php echo $message;?></div>
 
-<?php echo form_open("auth/change_password");?>
+    <?php echo form_open("auth/change_password");?>
 
-      <p>
-            <?php echo lang('change_password_old_password_label', 'old_password');?> <br />
-            <?php echo form_input($old_password);?>
-      </p>
+          <p>
+                <?php echo lang('change_password_old_password_label', 'old_password');?> <br />
+                <?php echo form_input($old_password, '', ["class" => "form-control"]);?>
+          </p>
 
-      <p>
-            <label for="new_password"><?php echo sprintf(lang('change_password_new_password_label'), $min_password_length);?></label> <br />
-            <?php echo form_input($new_password);?>
-      </p>
+          <p>
+                <label for="new_password"><?php echo sprintf(lang('change_password_new_password_label'), $min_password_length);?></label> <br />
+                <?php echo form_input($new_password, '', ["class" => "form-control"]);?>
+          </p>
 
-      <p>
-            <?php echo lang('change_password_new_password_confirm_label', 'new_password_confirm');?> <br />
-            <?php echo form_input($new_password_confirm);?>
-      </p>
+          <p>
+                <?php echo lang('change_password_new_password_confirm_label', 'new_password_confirm');?> <br />
+                <?php echo form_input($new_password_confirm, '', ["class" => "form-control"]);?>
+          </p>
 
-      <?php echo form_input($user_id);?>
-      <p><?php echo form_submit('submit', lang('change_password_submit_btn'));?></p>
+          <?php echo form_input($user_id);?>
+          <p><?php echo form_submit('submit', lang('change_password_submit_btn'), ["class" => "btn btn-primary"]);?></p>
 
-<?php echo form_close();?>
+    <?php echo form_close();?>
+</div>
+<?php $this->load->view('layout/footer'); ?>
