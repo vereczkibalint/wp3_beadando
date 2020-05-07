aa8febc8a0b6d3b795f412a87a15ec76f8dfb049
vereczkibalint <vereczkibalint@gmail.com>
2020-05-07 14:56:50 +0200

    some styling to login site

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,29 +1,41 @@
 <?php $this->load->view('layout/header'); ?>
-<?php $this->load->view('layout/navbar'); ?>
-<div class='text-center'>
-    <?php echo form_open('login', array('class'=>'login-form'));?>
+<div class='text-center loginBox'>
+    <div id="infoMessage"><?php echo $message;?></div>
+    <?php echo form_open('auth/login', array('class'=>'login-form'));?>
+    
         <h1 class="h3 mb-3 font-weight-normal">Bejelentkezés</h1>
+        
         <?php echo form_label('Email:','email', array('class'=>'sr-only'));?>
-        <?php echo form_input('email',set_value('email'), array(
+        <?php echo form_input($identity,set_value('email'), array(
             'type'=>'email',
             'id'=>'email',
             'class' => 'form-control',
             'placeholder'=>'Email cím'
         ));?>
         <?php echo form_error('email');?>
-<br>
+        
+        <br>
+        
         <?php echo form_label('Password:', 'password', array('class'=>'sr-only'));?>
-        <?php echo form_password('password','', array(
+        <?php echo form_password($password,'', array(
             'type'=>'password',
             'id'=>'password',
             'class' => 'form-control',
             'placeholder'=>'Jelszó'
         ));?>
         <?php echo form_error('password');?>
-<br>
-        <?php echo form_submit('login','Bejelentkezés',array(
-            'class'=>'btn btn-lg btn-primary btn-block'
+        
+        <br>
+        
+        <?php echo lang('login_remember_label', 'remember');?>
+        <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+        
+        <br>
+        
+        <?php echo form_submit('submit','Bejelentkezés',array(
+            'class'=>'btn btn-primary'
         )); ?>
+        
     <?php echo form_close();?>
 </div>
 <?php $this->load->view('layout/footer'); ?>
