174cbc7b53a2dff938edac233fe041a34c1df90b
vereczkibalint <vereczkibalint@gmail.com>
2020-05-08 11:56:27 +0200

    order listing fixed

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -6,9 +6,11 @@ class Order_model extends CI_Model {
     }
     
     public function get_list(){
-        $this->db->select('orders.order_id, orders.order_date, order_details.item_id, order_details.quantity');
+        $this->db->select('orders.order_id, orders.order_date, items.name, order_details.quantity, sum(items.price * order_details.quantity)');
         $this->db->from('orders');
         $this->db->join('order_details', 'orders.order_id = order_details.order_id');
+        $this->db->join('items', 'order_details.item_id = items.id');
+        $this->db->group_by('orders.order_id');
         $this->db->order_by('order_date', 'ASC');
        
         $query = $this->db->get();
