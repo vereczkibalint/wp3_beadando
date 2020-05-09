a9a6109ac0bef6808c36c38e35b774b8ae1fee2a
vereczkibalint <vereczkibalint@gmail.com>
2020-05-09 22:41:38 +0200

    added styling

--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -1,3 +1,4 @@
+<?php $this->load->view('layout/header'); ?>
 <h1><?php echo lang('create_user_heading');?></h1>
 <p><?php echo lang('create_user_subheading');?></p>
 
@@ -7,12 +8,12 @@
 
       <p>
             <?php echo lang('create_user_fname_label', 'first_name');?> <br />
-            <?php echo form_input($first_name);?>
+            <?php echo form_input($first_name, '', ["class" => "form-control"]);?>
       </p>
 
       <p>
             <?php echo lang('create_user_lname_label', 'last_name');?> <br />
-            <?php echo form_input($last_name);?>
+            <?php echo form_input($last_name, '', ["class" => "form-control"]);?>
       </p>
       
       <?php
@@ -21,37 +22,38 @@
           echo lang('create_user_identity_label', 'identity');
           echo '<br />';
           echo form_error('identity');
-          echo form_input($identity);
+          echo form_input($identity, '', ["class" => "form-control"]);
           echo '</p>';
       }
       ?>
 
       <p>
             <?php echo lang('create_user_company_label', 'company');?> <br />
-            <?php echo form_input($company);?>
+            <?php echo form_input($company, '', ["class" => "form-control"]);?>
       </p>
 
       <p>
             <?php echo lang('create_user_email_label', 'email');?> <br />
-            <?php echo form_input($email);?>
+            <?php echo form_input($email, '', ["class" => "form-control"]);?>
       </p>
 
       <p>
             <?php echo lang('create_user_phone_label', 'phone');?> <br />
-            <?php echo form_input($phone);?>
+            <?php echo form_input($phone, '', ["class" => "form-control"]);?>
       </p>
 
       <p>
             <?php echo lang('create_user_password_label', 'password');?> <br />
-            <?php echo form_input($password);?>
+            <?php echo form_input($password, '', ["class" => "form-control"]);?>
       </p>
 
       <p>
             <?php echo lang('create_user_password_confirm_label', 'password_confirm');?> <br />
-            <?php echo form_input($password_confirm);?>
+            <?php echo form_input($password_confirm, '', ["class" => "form-control"]);?>
       </p>
 
 
-      <p><?php echo form_submit('submit', lang('create_user_submit_btn'));?></p>
+      <p><?php echo form_submit('submit', lang('create_user_submit_btn'), ["class" => "btn btn-primary"]);?></p>
 
 <?php echo form_close();?>
+<?php $this->load->view('layout/footer'); ?>
