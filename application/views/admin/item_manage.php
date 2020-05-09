990449c34a106538353615d0a32c13e5a895f4b8
vereczkibalint <vereczkibalint@gmail.com>
2020-05-09 22:21:43 +0200

    page for item management created

---
+++ application/views/admin/item_manage.php
@@ -0,0 +1,20 @@
+<div class="adminPanelBox mt-3">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <article class="card-group-item">
+                    <header class="card-header"><h6 class="title">Admin panel</h6></header>
+                    <div class="filter-content">
+                        <div class="list-group list-group-flush">
+                        <a href="<?=base_url('admin/manage/users')?>" class="list-group-item">Felhasználók kezelése</a>
+                        <a href="<?=base_url('admin/manage/items')?>" class="list-group-item active">Termékek kezelése</a>
+                        </div> 
+                    </div>
+                </article>
+            </div> <!-- .card -->
+        </div> <!-- .col-md-4 -->
+        <div class="col-md-8">
+            
+        </div>
+    </div> <!-- .row -->
+</div> <!-- .adminPanelBox -->
