c288a4be735f33b0c648b83b9b5af441441aa5d0
vereczkibalint <vereczkibalint@gmail.com>
2020-05-10 15:12:14 +0200

    orders are listed in a table

--- application/views/orders/list.php
+++ application/views/orders/list.php
@@ -1,5 +1,26 @@
 <?php if(isset($orders) && !empty($orders)): ?>
-<?php var_dump($orders); ?>
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Rendelés azonosítója</th>
+                <th>Rendelés dátuma</th>
+                <th>Végösszeg</th>
+                <th>Műveletek</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($orders as $order): ?>
+            <tr>
+                <td><?=$order['order_id']?></td>
+                <td><?=$order['order_date']?></td>
+                <td><?=$order['sumprice']?> Ft</td>
+                <td><a href="<?=base_url('orders/details/'.$order['order_id'])?>">Részletek</a></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
 <?php else: ?>
 <h1 class="mt-2 text-center">Nincs megjeleníthető rendelés!</h1>
 <?php endif; ?>
