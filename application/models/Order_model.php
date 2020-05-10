ff02e7cf36c9d32ea226fabf78cd2e60d8622ec6
vereczkibalint <vereczkibalint@gmail.com>
2020-05-10 15:10:40 +0200

    fixed group by issue and created fetch order by id method

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -3,10 +3,11 @@ class Order_model extends CI_Model {
     public function __construct() {
         parent::__construct();
         $this->load->database();
+        $this->load->library('ion_auth');
     }
     
     public function get_list(){
-        $this->db->select('orders.order_id, orders.order_date, items.name, order_details.quantity, sum(items.price * order_details.quantity)');
+        $this->db->select('orders.order_id, orders.order_date, sum(items.price * order_details.quantity)');
         $this->db->from('orders');
         $this->db->join('order_details', 'orders.order_id = order_details.order_id');
         $this->db->join('items', 'order_details.item_id = items.id');
@@ -21,12 +22,26 @@ class Order_model extends CI_Model {
     }
     
     public function get_record_by_userid($userid){
-        $this->db->select('orders.order_id, orders.order_date, order_details.item_id, order_details.quantity');
+        $this->db->select('orders.order_id, orders.order_date, sum(items.price * order_details.quantity) as sumprice');
         $this->db->from('orders');
         $this->db->join('order_details', 'orders.order_id = order_details.order_id');
+        $this->db->join('items', 'order_details.item_id = items.id');
         $this->db->order_by('order_date', 'ASC');
         $this->db->where('orders.user_id', $userid);
+        $this->db->group_by('orders.order_id');
         
-        return $this->db->get()->row();
+        return $this->db->get()->result_array();
+    }
+    
+    public function get_order_by_id($orderid){
+        $this->db->select('orders.order_id, items.name, orders.order_date, sum(items.price * order_details.quantity) as sumprice');
+        $this->db->from('orders');
+        $this->db->join('order_details', 'orders.order_id = order_details.order_id');
+        $this->db->join('items', 'order_details.item_id = items.id');
+        $this->db->order_by('order_date', 'ASC');
+        $this->db->where('orders.order_id', $orderid);
+        $this->db->group_by('orders.order_id');
+        
+        return $this->db->get()->result_array();
     }
 }
