cb22233094fe9c33cadc55f8ca82cfb946fe7c15
vereczkibalint <vereczkibalint@gmail.com>
2020-05-11 22:35:54 +0200

    created cart model

---
+++ application/models/Cart_model.php
@@ -0,0 +1,41 @@
+<?php
+class Cart_model extends CI_Model{
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    public function get_cart(){
+        if(!$this->ion_auth->logged_in()){
+            show_error('Ehhez a funkcióhoz be kell jelentkezni!');
+        }else{
+            $this->db->select('cart.cart_id, cart.item_id, cart.user_id, items.name, items.price, items.image');
+            $this->db->from('cart');
+            $this->db->join('items', 'cart.item_id = items.id');
+            $this->db->where('cart.user_id', $this->ion_auth->get_user_id());
+
+            $query = $this->db->get();
+
+            $cart = $query->result_array();
+
+            return $cart;
+        }
+    }
+    
+    public function add_to_cart($itemid){
+        $insert = [
+            'user_id' => $this->ion_auth->user()->row()->id,
+            'item_id' => $itemid
+        ];
+        
+        return $this->db->insert('cart',$insert);
+    }
+    
+    public function remove_from_cart($item_id){
+        return $this->db->delete('cart', ['item_id' => $item_id]);
+    }
+    
+    public function empty_cart(){
+        return $this->db->delete('cart', ['user_id' => $this->ion_auth->get_user_id()]);
+    }
+}
