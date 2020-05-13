93a2b03a5b7fb5ee23768652681416fe90df41a0
vereczkibalint <vereczkibalint@gmail.com>
2020-05-13 22:13:02 +0200

    added menu exporting

---
+++ application/controllers/Export.php
@@ -0,0 +1,23 @@
+<?php
+class Export extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('Pizza_model', 'pizza');
+    }
+    
+    public function export_menu(){
+        $pizzas = $this->pizza->get_list();
+         
+        $fp = fopen('php://output', 'w+'); 
+        header('Content-type: application/octet-stream');  
+        header('Content-disposition: attachment; filename="menu.csv"'); 
+        fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
+        
+        foreach($pizzas as $pizza){
+            fputcsv($fp, [$pizza['name'], $pizza['price'] . 'Ft'], ';', ' ');
+        }
+        
+        fclose($fp);
+        exit();
+    }
+}
