6726986ab6e8b1c0782133ac383f967d75094468
vereczkibalint <vereczkibalint@gmail.com>
2020-05-14 15:56:04 +0200

    added header to menu

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -11,7 +11,10 @@ class Export extends CI_Controller{
         $fp = fopen('php://output', 'w+'); 
         header('Content-type: application/octet-stream');  
         header('Content-disposition: attachment; filename="menu.csv"'); 
-        fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF));
+        
+        fprintf($fp, chr(0xEF).chr(0xBB).chr(0xBF)); // UTF-8 fix
+        $header = array("Termék neve","Termék ára"); 
+        fputcsv($fp, $header,";");
         
         foreach($pizzas as $pizza){
             fputcsv($fp, [$pizza['name'], $pizza['price'] . 'Ft'], ';', ' ');
