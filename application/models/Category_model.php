215c9d08b0041346f2bcff45a0cc75bcd44c10c5
vereczkibalint <vereczkibalint@gmail.com>
2020-05-14 15:56:57 +0200

    added category create function if import has a new category

--- application/models/Category_model.php
+++ application/models/Category_model.php
@@ -13,4 +13,22 @@ class Category_model extends CI_Model{
         
         return $query->result();
     }
+    
+    public function get_id_by_name($category_name){
+        $this->db->select('category_id');
+        $this->db->from('item_categories');
+        $this->db->where('category_name', $category_name);
+        
+        return $this->db->get()->row();
+    }
+    
+    public function create($category_name){
+        $category = [
+            'category_name' => $category_name
+        ];
+        
+        $this->db->insert('item_categories', $category);
+        
+        return $this->db->insert_id();
+    }
 }
